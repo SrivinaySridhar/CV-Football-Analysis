4ee777301677d76c19d0a22b80cc9139acd8851e
SrivinaySridhar <srivinay.sridhar@gmail.com>
2024-08-08 15:21:35 +0530

    Inference and data for training

---
+++ yolo_inference.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+
+model = YOLO('yolov8x')
+
+video_path = "input_videos/08fd33_4.mp4"
+
+results = model.predict(video_path, save = True)
+
+print(results[0])
+
+print("==================================================")
+
+for box in results[0].boxes:
+    print(box)
