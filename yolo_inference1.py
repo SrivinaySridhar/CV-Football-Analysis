c925fa2e4c7469bd80a964bf26cfe815eefb6bad
SrivinaySridhar <srivinay.sridhar@gmail.com>
2024-08-13 12:45:39 +0530

    Rerun of yolo inference

--- yolo_inference1.py
+++ yolo_inference1.py
@@ -4,7 +4,7 @@ model = YOLO('models/best.pt')
 
 video_path = "input_videos/08fd33_4.mp4"
 
-results = model.predict(video_path, save = True)
+results = model.predict(video_path, save = True, project = "runs/detect", name = "predict2")
 
 print(results[0])
 
